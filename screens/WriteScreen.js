70ce6a2b76d1bbc099eda241dc7bbdc4e0860908
Apple <apple@Apples-MacBook-Pro.local>
2020-07-03 19:56:05 +0530

    added text input

--- screens/WriteScreen.js
+++ screens/WriteScreen.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
-import {Text, View} from 'react-native';
+import {Text, View, TextInput, TouchableOpacity} from 'react-native';
 
 export default class WriteScreen extends Component{
+    constructor(){
+        super()
+        this.state={
+            text:''
+        }
+    }
     render(){
         return(
-            <View>
-                <Text>
-                    Write screen
-                </Text>
+            <View style={{flex:1, justifyContent:'center', alignItems:'center'}}>
+              <View>
+                <TextInput style={{height:500, width:500, borderColor:'black', borderWidth:1}}
+                onChangeText={(text)=>{
+this.setState({
+    text:text
+})
+                }}
+                value={this.state.text}
+                />
+                <TouchableOpacity style={{backgroundColor:'black', padding:10, margin:10}}>
+                    Submit button
+                </TouchableOpacity>
+                </View>
             </View>
         )
     }
