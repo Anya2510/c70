e6f98c6b56c83074eec6e69cb88a31c132ccc35f
Apple <apple@Apples-MacBook-Pro.local>
2020-07-03 19:39:54 +0530

    stage 1

---
+++ screens/WriteScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import {Text, View} from 'react-native';
+
+export default class WriteScreen extends Component{
+    render(){
+        return(
+            <View>
+                <Text>
+                    Write screen
+                </Text>
+            </View>
+        )
+    }
+}
